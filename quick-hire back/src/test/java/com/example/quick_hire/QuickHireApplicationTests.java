cdf3740b54e54fe50b0afc210145f68bf13bbcdb
kossovar <qmgharris@icloud.com>
2025-04-27 14:05:07 +0200

    Initialize Spring Boot project and React template

---
+++ quick-hire back/src/test/java/com/example/quick_hire/QuickHireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quick_hire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickHireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
